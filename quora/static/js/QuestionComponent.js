6f321287e64bbc41fcbc66e4986ba694ba9f81bf
sanix sandel <sanicksikani@gmail.com>
2020-06-30 22:20:23 +0300

    Question Component rendered

---
+++ quora/static/js/QuestionComponent.js
@@ -0,0 +1,42 @@
+function QuestionBlock(){
+    var block="<div class='ui-block'>"+
+    "<article class='hentry post'>"+
+       "<div class='m-link'>"+
+          "<a href='#'  target='_blank'>"+
+            "<a href='#'><h4 id='#title'></h4></a>"+
+          "</a>"+
+       "</div>"+
+       "<div class='post__author author vcard inline-items'>"+
+          "<img src='#'>"+
+          "<div class='author-date'>"+
+            "<a class='h6 post__author-name fn' href='#'></a>"+
+             "<div class='post__date'>"+
+                "<time class='published' datetime='2004-07-24T18:18'>"+
+                "Asked 2h ago"+
+                "</time>"+
+             "</div>"+
+          "</div>"+
+          "<div class='more'>"+
+            "<a href='#'>"+
+            "<i class='fa fa-ellipsis-v'></i>"+
+             "</a>"+
+          "</div>"+
+       "</div>"+
+       "<p>"+
+         
+       "</p>"+
+       "<div class='post-additional-info inline-items'>"+
+          "<p>"+
+             "<a href='#' class='btn btn-sm btn-light'><span class='fa fa-pencil'></span> Answer</a>"+
+             "<a href='#' class='btn btn-sm btn-light'><i class='fa fa-retweet'></i> Retweeter</a>"+
+          "</p>"+
+          "<p class='social-icons'>"+
+             "<a href='#' class='btn btn-sm btn-light'><i class='fa fa-facebook'></i></a>"+
+             "<a href='#' class='btn btn-sm btn-light'> <i class='fa fa-twitter'></i></a>"+
+             "<a href='#' class='btn btn-sm btn-light'> <i class='fa fa-share'></i></a>"+
+          "</p>"+
+       "</div>"+
+    "</article>"+
+  "</div>"
+    return block;
+}
